cdf3a66840e2cb3e313fd23dcf136cde4a5b554c
Mardon_M <mardonov.mexriddin1997@gmail.com>
2025-04-18 10:21:08 +0500

    gulp set up

---
+++ src/js/index.js
@@ -0,0 +1,55 @@
+
+//  Подключение посторонних библиотек через npm в консоле
+// import Splide from '@splidejs/splide';
+// import '@splidejs/splide/css'; // базовые стили
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   // Sliders
+//   const sliders = () => {
+//     new Splide('.splide__slider', {
+//       type: 'loop',
+//       perPage: 2,
+//       breakpoints: {
+//         992: {
+//           perPage: 1,
+//           arrows: false,
+//         },
+//       },
+//       arrows: true,
+//       pagination: false,
+//       gap: '3px',
+//     }).mount();
+  
+//     new Splide('.splide__parthners', {
+//       type: 'loop',
+//       perPage: 5,
+//       breakpoints: {
+//         992: {
+//           perPage: 1,
+//         },
+//       },
+//       arrows: false,
+//       pagination: false,
+//       gap: '3px',
+//     }).mount();
+  
+//     new Splide('.splide__quality', {
+//       type: 'slide',
+//       perPage: 4,
+//       breakpoints: {
+//         992: {
+//           perPage: 1,
+//         },
+//       },
+//       arrows: false,
+//       pagination: false,
+//       gap: '3px',
+//     }).mount();
+//   }
+//   if (window.location.pathname === '/') {
+//     sliders();
+//   }
+// });
+
+
+console.log('Gulp Webpack сборка работает!');
